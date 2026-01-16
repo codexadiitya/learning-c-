aa090262e6dae92a088d5cdf528def1e8c95c7c8
Aditya Diwan <adityadiwa2005@gmail.com>
2026-01-16 23:11:52 +0530

    Added conditionals and loops programs

---
+++ conditional&loops.cpp/postivenegativezzero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num = -10;
+
+  if (num > 0)
+  {
+    cout << "postive number" << endl;
+  }
+  else if (num < 0)
+  {
+    cout << "negative number" << endl;
+  }
+  else
+  {
+    cout << "zero" << endl;
+  }
+
+  return 0;
+}
