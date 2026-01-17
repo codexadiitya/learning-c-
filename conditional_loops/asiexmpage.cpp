8e2654bcc8c3870a19368ae157f55c3a2ebaa3f2
Aditya Diwan <adityadiwa2005@gmail.com>
2026-01-17 12:00:27 +0530

    Added C++ programs on loops and conditions

---
+++ conditional_loops/asiexmpage.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int age = 10;
+  if (age < 10)
+  {
+    cout << "hello ji" << endl;
+  }
+  cout << " hy evreyone" << endl;
+}
--- printbasic.cpp/1.cpp
+++ printbasic.cpp/1.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 int main()
 {
+ 
+ 
   int num;  // Declare num without initialization
   num = -5; // Initialize num to 50
   cout << sizeof(num) << endl;
   cout << num << endl;
+
+  
+
 
   char ch = 'A';
   cout << ch << endl;
